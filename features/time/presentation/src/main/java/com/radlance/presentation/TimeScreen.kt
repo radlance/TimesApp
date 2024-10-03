249c3d5b3db41c4727173e425b65c33dedbf188a
Dmitry <manyakindima@gmail.com>
2024-10-03 19:33:15 +0300

    update time format

--- features/time/presentation/src/main/java/com/radlance/presentation/TimeScreen.kt
+++ features/time/presentation/src/main/java/com/radlance/presentation/TimeScreen.kt
@@ -1,5 +1,6 @@
 package com.radlance.presentation
 
+import android.text.format.DateFormat
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,9 +52,9 @@ fun TimeScreen(
                         Spacer(modifier = Modifier.height(24.dp))
 
                         DigitalClockComponent(
-                            hour = hour.toString(),
-                            minute = minute.toString(),
-                            seconds = seconds.toString(),
+                            hour = hour,
+                            minute = minute,
+                            seconds = seconds,
                             timeZone = timeZone
                         )
                     }
@@ -64,17 +66,26 @@ fun TimeScreen(
 
 @Composable
 fun DigitalClockComponent(
-    hour: String,
-    minute: String,
-    seconds: String,
+    hour: Int,
+    minute: Int,
+    seconds: Int,
     timeZone: String
 ) {
-    val formattedHour = if (hour.length == 1) "0$hour" else hour
-    val formattedMinute = if (minute.length == 1) "0$minute" else minute
-    val formattedSeconds = if (seconds.length == 1) "0$seconds" else seconds
+    val is24HourFormat = DateFormat.is24HourFormat(LocalContext.current)
+
+    val formattedHour = if (is24HourFormat) {
+        if (hour < 10) "0$hour" else "$hour"
+    } else {
+        val hour12 = if (hour == 0 || hour == 12) 12 else hour % 12
+        if (hour12 < 10) "0$hour12" else "$hour12"
+    }
+
+    val formattedMinute = if (minute < 10) "0$minute" else "$minute"
+    val formattedSeconds = if (seconds < 10) "0$seconds" else "$seconds"
+    val amPm = if (is24HourFormat) "" else if (hour < 12) "AM" else "PM"
 
     Text(
-        text = "$formattedHour:$formattedMinute:$formattedSeconds",
+        text = "$formattedHour:$formattedMinute:$formattedSeconds $amPm",
         style = MaterialTheme.typography.displayMedium
     )
     Spacer(Modifier.height(8.dp))
